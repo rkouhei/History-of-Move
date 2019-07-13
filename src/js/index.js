f1fcabd5f6941947a2e7aa238187a9c346cf95b0
RKouhei <kohriki222@gmail.com>
2019-07-14 05:23:18 +0900

    read csv and display date

--- src/js/index.js
+++ src/js/index.js
@@ -1,33 +1,46 @@
 import dotenv from 'dotenv'
-// import csvParse from 'csv-parse'
-import fs from 'fs'
-import path from 'path'
-dotenv.config();
+const csv = require('csv-parser/')
+dotenv.config()
 
-let pointlat = 34.2929505;
-let pointlon = 134.061257;
+function datadisplay(results) {
+    let element = document.createElement('b') // 挿入するタグ
+    let inspos = document.getElementById('csv') // 挿入する位置をidで決める
+    let text = results[0]['time']
+    text = text.split(' ')
+    element.innerHTML = text[0] + 'の行動履歴'
+    inspos.appendChild(element)
+    console.log('debug');
+}
 
-const mymap = L.map('mapid').setView([pointlat, pointlon], 13);
+let pointlat = 34.2929505
+let pointlon = 134.061257
+
+const mymap = L.map('mapid').setView([pointlat, pointlon], 13)
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.streets', 
     accessToken: process.env.ACCESS_TOKEN
-}).addTo(mymap);
+}).addTo(mymap)
 
-const csv = require('csv-parser/');
-const stream = csv();
 
+const stream = csv()
+// 1行ずつ処理
+let results = []
 stream.on('data', function(data) {
-    console.log(data)
-});
+    results.push(data)
+})
 
+// データの読み込み
 document.querySelector('input')
-    .addEventListener('change', (e) => {
+    .addEventListener('change', (e) => { // ファイルが入力されると呼ばれる
         const reader = new FileReader
         reader.onload = (e) => {
-            stream.write(e.target.result)
+            stream.write(e.target.result) // stream.onの処理を行う
+            // console.log(results[0]['time'])
+            datadisplay(results)
+            // console.log(results.length)
         }
         
         if (e.target.files[0]) {
@@ -35,14 +48,6 @@ document.querySelector('input')
             reader.readAsText(file)
         }
     });
-
-// let csvdir = path.resolve(__dirname, '/log_2019-05-23.csv');
-// console.log(csvdir);
-// fs.createReadStream(csvdir)
-//     .pipe(csv.parse({columns: true}, function(err, data) {
-
-//         console.log(JSON.stringify(data));
-//     }));
 
 // マーカー
 // var marker = L.marker([pointlat, pointlon]).addTo(mymap);
